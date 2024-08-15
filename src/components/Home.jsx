7146f4e55ad0615b85ddc704ba76f2c08ef010fd
eliplutchok <eliplutchok1@gmail.com>
2024-08-15 01:10:01 -0400

    made text on home component bolder and made line height larger :)

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -17,7 +17,6 @@ const Home = () => {
               <li>This is your home page.</li>
               <li>The site is currently under development.</li>
               <li>Some pages and functionalities are not fully built out yet.</li>
-              <li>Use the navbar to navigate to existing pages.</li>
               <li>Admins have access to additional features and pages.</li>
               <li>There may be some bugs. If the site is not responding, try refreshing the page.</li>
               <li>Thank you for visiting!</li>
