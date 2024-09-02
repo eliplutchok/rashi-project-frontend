21d3dbc07916f5c2c8a43db278bbb893e52b6784
eliplutchok <eliplutchok1@gmail.com>
2024-09-02 15:42:20 -0400

    adjusted loading animation for talmud queries

--- src/components/QueryTalmud/LoadingAnimation.jsx
+++ src/components/QueryTalmud/LoadingAnimation.jsx
@@ -3,8 +3,9 @@ import React from 'react';
 const LoadingAnimation = ({ elapsedTime }) => (
     <div className="loader-container">
         <div className="scroll-loader">
+        <div className="seconds">{elapsedTime.toFixed(2)}</div>
             <div className="scroll-text">
-                <div className="seconds">{elapsedTime.toFixed(2)}</div>
+                
                 <div className="message">Queries can take anywhere from 5 to 20 seconds to process.</div>
             </div>
         </div>
